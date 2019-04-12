ddeafe01ed10d30a1a87a070fb9ef95ecf0fbbdf
ajm <ajm257@njit.edu>
2019-04-11 22:19:09 -0400

    Test 9: Unit test to test inserting a car - success.

---
+++ tests/Unit/CarTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Cars;
+
+class CarTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testInsertCars()
+    {
+
+        $record = new Cars();
+        $record->user_id='5';
+        $record->model='ford';
+        $record->make='taurus';
+        $record->year='2006';
+
+        $this->assertTrue($record->save());
+
+    }
+}
